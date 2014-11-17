5228bb344351aa88d84dd1c8e73799a6c71ccb0d
Mika Haapakorpi <mika.haapakorpi@gmail.com>
2014-11-17 10:01:34 +0200

    tietokantakantaskriptit, jotka luovat juku-tietokannan * taulualueet: juku_data ja juku_indx * skeeman: juku * sovelluskäyttäjän: juku_app

---
+++ juku-db/users/tablespace.sql
@@ -0,0 +1,10 @@
+
+define DATAFILE_PATH = '/home/oracle/app/oracle/oradata/orcl'
+
+create tablespace juku_data datafile 
+  '&DATAFILE_PATH/juku_data01.dbf' size 40 m reuse autoextend on next 1 m maxsize 2G
+;
+
+create tablespace juku_indx datafile 
+  '&DATAFILE_PATH/juku_indx01.dbf' size 40 m reuse autoextend on next 1 m maxsize 2G
+;
---
+++ juku-db/users/users.sql
@@ -0,0 +1,49 @@
+
+create role livi_schema not identified;
+
+grant alter session, 
+  alter system, 
+  create cluster, 
+  create database link, 
+  create indextype, 
+  create materialized view, 
+  create operator, 
+  create procedure,
+  create sequence, 
+  create session, 
+  create synonym, 
+  create table, 
+  create trigger, 
+  create type, 
+  create view
+to livi_schema 
+;
+
+create role livi_application not identified;
+
+grant create session to livi_application;
+
+create user juku 
+    identified by juku 
+    default tablespace juku_data 
+    quota unlimited on juku_data 
+    quota unlimited on juku_indx 
+    account unlock 
+;
+
+create user juku_app 
+    identified by juku 
+    account unlock 
+;
+
+grant livi_schema to juku;
+grant livi_application to juku_app;
+
+-- set default schema to juku application
+create or replace trigger juku_app.set_default_schema
+after logon on juku_app.schema
+begin
+  execute immediate 'alter session set current_schema = juku';
+end;
+
+
