85525ecde8c764d733c9292c14a4e32f0ececac2
Mika Haapakorpi <mika.haapakorpi@gmail.com>
2014-11-20 21:16:34 +0200

    oikeat toimivaltaiset viranomaiset

--- juku-db/sql/incremental/01_v1.0/006_toimivaltaiset-viranomaiset.sql
+++ juku-db/sql/incremental/01_v1.0/006_toimivaltaiset-viranomaiset.sql
@@ -1,2 +1,38 @@
 
-insert into osasto (id, nimi) values (osasto_seq.nextval, 'Hervanta');
+declare
+  type OsastoList is table of osasto%rowtype;
+
+  function new_osasto(nimi varchar2) return osasto%rowtype is
+    o osasto%rowtype;
+  begin
+    o.nimi := nimi;
+    return o;
+  end;
+
+  procedure save_osastot(osastot OsastoList) is
+  begin
+    forall i in osastot.first .. osastot.last
+      insert into osasto (id, nimi) values
+        (osasto_seq.nextval, osastot(i).nimi);
+  end;
+begin
+
+save_osastot(OsastoList(
+  new_osasto('Helsingin seudun liikenne'),
+  new_osasto('Hämeenlinna'),
+  new_osasto('Joensuu'),
+  new_osasto('Jyväskylä'),
+  new_osasto('Kotka'),
+  new_osasto('Kouvola'),
+  new_osasto('Kuopio'),
+  new_osasto('Lahti'),
+  new_osasto('Lappeenranta'),
+  new_osasto('Oulu'),
+  new_osasto('Pori'),
+  new_osasto('Tampere'),
+  new_osasto('Turku'),
+  new_osasto('Vaasa')
+));
+
+end;
+/
