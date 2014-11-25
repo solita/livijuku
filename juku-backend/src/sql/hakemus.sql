3d30fd7a4030da3c6de78eb8e87d75872982ac98
Mika Haapakorpi <mika.haapakorpi@gmail.com>
2014-11-25 23:07:22 +0200

    perusinserttiä ei tarvitse kirjoittaa käsin

--- juku-backend/src/sql/hakemus.sql
+++ juku-backend/src/sql/hakemus.sql
@@ -2,11 +2,3 @@
 -- name: select-osaston-hakemukset
 select id, vuosi, nro, hakuaika_alkupvm, hakuaika_loppupvm
 from hakemus where osastoid = :osastoid
-
-
--- name: insert-hakemus<!
--- Lisää hakemus, palauta id.
-insert into hakemus (id, vuosi, nro,
-  hakuaika_alkupvm, hakuaika_loppupvm, osastoid)
-values (hakemus_seq.nextval, :vuosi, :nro,
-  :hakuaika_alkupvm, :hakuaika_loppupvm, :osastoid)
