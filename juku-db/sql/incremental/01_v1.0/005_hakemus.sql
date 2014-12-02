d9598292458c21a007ce2f0b0eb3d530d7d91c6e
Mika Haapakorpi <mika.haapakorpi@gmail.com>
2014-12-02 14:31:46 +0200

    Tietomallipalaverin muutoksia: hakemuksen järjestysnumero (nro) muutetaan hakemustyypiksi

--- juku-db/sql/incremental/01_v1.0/005_hakemus.sql
+++ juku-db/sql/incremental/01_v1.0/005_hakemus.sql
@@ -6,11 +6,20 @@ create sequence hakemus_seq
     cache 20
 ;
 
+begin
+  model.new_classification('hakemustyyppi', 'Hakemustyyppi', 3, 'HATYYPPI');
+end;
+/
+
+insert into hakemustyyppi (tunnus, nimi) values ('AH0', 'Avustushakemus');
+insert into hakemustyyppi (tunnus, nimi) values ('MH1', '1. maksatushakemus');
+insert into hakemustyyppi (tunnus, nimi) values ('MH2', '2. maksatushakemus');
+
 create table hakemus (
   id number constraint hakemus_pk primary key,
   vuosi number(4) not null,
   organisaatioid not null references organisaatio (id),
-  nro number(1),
+  hakemustyyppitunnus not null references hakemustyyppi (tunnus),
   hakuaika_alkupvm date not null,
   hakuaika_loppupvm date not null,
 
