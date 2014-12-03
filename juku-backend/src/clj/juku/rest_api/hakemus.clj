eac7a43d4e4b670825977c1cfad3a63628680c29
Mika Haapakorpi <mika.haapakorpi@gmail.com>
2014-12-03 13:26:41 +0200

    fix return type for /hakemus/avustuskohteet/:hakemusid

--- juku-backend/src/clj/juku/rest_api/hakemus.clj
+++ juku-backend/src/clj/juku/rest_api/hakemus.clj
@@ -1,6 +1,6 @@
 (ns juku.rest-api.hakemus
   (:require [compojure.api.sweet :refer :all]
-            [juku.service.hakemus :as db]
+            [juku.service.hakemus :as service]
             [juku.schema.hakemus :refer :all]
             [ring.util.http-response :refer :all]
             [ring.swagger.schema :refer [describe]]
@@ -11,25 +11,25 @@
             :return [Hakemuskausi]
             :path-params [organisaatioid :- Long]
             :summary "Hae hakijan hakemukset hakemuskausittain (vuosittain) ryhmitettynä."
-            (ok (db/find-organisaation-hakemukset-vuosittain organisaatioid)))
+            (ok (service/find-organisaation-hakemukset-vuosittain organisaatioid)))
       (GET* "/hakemus/:hakemusid" []
             :return [Hakemus]
             :path-params [hakemusid :- Long]
             :summary "Hae hakemuksen perustiedot. Haettava hakemus yksilöidään hakemusid-polkuparametrilla."
-            (ok (db/get-hakemus-by-id hakemusid)))
+            (ok (service/get-hakemus-by-id hakemusid)))
       (GET* "/hakemus/avustuskohteet/:hakemusid" []
-            :return [Hakemus]
+            :return [Avustuskohde]
             :path-params [hakemusid :- Long]
             :summary "Hae hakemuksen avustuskohteet. Haettava hakemus yksilöidään hakemusid-polkuparametrilla."
-            (ok (db/find-avustuskohteet-by-hakemusid hakemusid)))
+            (ok (service/find-avustuskohteet-by-hakemusid hakemusid)))
       (POST* "/hakemus" []
              :return   s/Num
              :body     [hakemus New-Hakemus]
              :summary  "Lisää yksittäinen hakemus"
-             (ok (db/add-hakemus! hakemus)))
+             (ok (service/add-hakemus! hakemus)))
       (POST* "/hakemuskausi" []
              :return   nil
              :form-params     [vuosi :- s/Int]
              :summary  "Avaa uusi hakemuskausi"
-             (ok (db/avaa-hakemuskausi! vuosi))))
+             (ok (service/avaa-hakemuskausi! vuosi))))
 
