2cfa629a10446cd7f915ecbf534042b3f8ba0f01
Mika Haapakorpi <mika.haapakorpi@gmail.com>
2014-12-04 23:35:48 +0200

    testidata - profiilissa test-data - käyttö: lein with-profiles +test-data do clear-db, update-db

--- juku-db/project.clj
+++ juku-db/project.clj
@@ -3,13 +3,16 @@
 	
 	:plugins [[lein-dbmaintain "0.1.3"] [oracle/ojdbc6 "11.2.0.3.0"]]
 
-	:dbmaintain {:driver "oracle.jdbc.driver.OracleDriver"
-				 :url "jdbc:oracle:thin:@localhost:1521:orcl"
-				 :user-name "juku"
-				 :password "juku"
-				 :schemas "juku"
-				 :scripts "sql"
-				 :dialect "oracle"}
+	:dbmaintain {
+			 :driver "oracle.jdbc.driver.OracleDriver"
+			 :url "jdbc:oracle:thin:@localhost:1521:orcl"
+			 :user-name "juku"
+			 :password "juku"
+			 :schemas "juku"
+			 :scripts "sql"
+			 :dialect "oracle"}
 				 
 	:repositories [["solita" {:url "http://mvn.solita.fi/archiva/repository/solita/" :snapshots true}]]
+
+	:profiles {:test-data {:dbmaintain {:scripts "sql, test/sql"}}}
 )
---
+++ juku-db/test/sql/repeatable/test/users.sql
@@ -0,0 +1,6 @@
+
+insert into kayttaja (tunnus, etunimi, sukunimi, organisaatioid, jarjestelma)
+  values ('harri', 'Harri', 'Hakija', 1, 0);
+  
+insert into kayttaja (tunnus, etunimi, sukunimi, organisaatioid, jarjestelma)
+  values ('katri', 'Katri', 'Käsittelijä', 1, 0);
