17adde90b3bb9fd61c5017b5b51ff71c0ab48994
Mika Haapakorpi <mika.haapakorpi@gmail.com>
2014-12-17 16:50:43 +0200

    lisätty liikennevirasto organisaatio ja katri käsittelijä kuuluu liikennevirastoon

---
+++ juku-db/sql/incremental/01_v1.0/007_organisaatiot.sql
@@ -0,0 +1,43 @@
+
+declare
+  type OrganisaatioList is table of organisaatio%rowtype;
+
+  function new_organisaatio(nimi varchar2, laji varchar2) return organisaatio%rowtype is
+    o organisaatio%rowtype;
+  begin
+    o.nimi := nimi;
+    o.lajitunnus := laji;
+    return o;
+  end;
+
+  procedure save_osastot(organisaatiot OrganisaatioList) is
+  begin
+    forall i in organisaatiot.first .. organisaatiot.last
+      insert into organisaatio (id, nimi, lajitunnus) values
+        (organisaatio_seq.nextval, organisaatiot(i).nimi, organisaatiot(i).lajitunnus);
+  end;
+begin
+
+save_osastot(OrganisaatioList(
+  -- toimivaltaiset viranomaiest --
+  new_organisaatio('Helsingin seudun liikenne', 'KS1'),
+  new_organisaatio('Hämeenlinna', 'KS2'),
+  new_organisaatio('Joensuu', 'KS2'),
+  new_organisaatio('Jyväskylä', 'KS2'),
+  new_organisaatio('Kotka', 'KS2'),
+  new_organisaatio('Kouvola', 'KS2'),
+  new_organisaatio('Kuopio', 'KS2'),
+  new_organisaatio('Lahti', 'KS2'),
+  new_organisaatio('Lappeenranta', 'KS2'),
+  new_organisaatio('Oulu', 'KS1'),
+  new_organisaatio('Pori', 'KS2'),
+  new_organisaatio('Tampere', 'KS1'),
+  new_organisaatio('Turku', 'KS1'),
+  new_organisaatio('Vaasa', 'KS2'),
+
+  -- liikennevirasto --
+  new_organisaatio('Liikennevirasto', 'LV')
+));
+
+end;
+/
