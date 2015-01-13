6c91e809eb849e6d6e708096a2a6b012110fb502
Petri Sirkkala <sirpete@iki.fi>
2015-01-13 15:57:53 +0200

    Depricated

--- juku-db/project.clj
+++ juku-db/project.clj
@@ -4,7 +4,7 @@
 	:plugins [[lein-dbmaintain "0.1.3"] [oracle/ojdbc6 "11.2.0.3.0"]]
 
 	:dbmaintain {
-			 :driver "oracle.jdbc.driver.OracleDriver"
+			 :driver "oracle.jdbc.OracleDriver"
 			 :url ~(str "jdbc:oracle:thin:@" (or (System/getenv "DB_URL") "localhost:1521:orcl"))
 			 :user-name ~(or (System/getenv "DB_USER") "juku")
 			 :password ~(or (System/getenv "DB_PASSWORD") "juku")
