cbc903be6a28df1e4c5d9e1e7528981fdec4fe29
Mika Haapakorpi <mika.haapakorpi@gmail.com>
2015-01-13 15:43:04 +0200

    documentation

--- juku-db/livi-kehitys-clear-db-update-db-test-data.sh
+++ juku-db/livi-kehitys-clear-db-update-db-test-data.sh
@@ -8,15 +8,7 @@ DB_PASSWORD=$2
 if [ -z $DB_USER ]; then usage; fi
 if [ -z $DB_PASSWORD ]; then usage; fi
 
-DB_URL="jdbc:oracle:thin:@$CONNECTION"
+CONNECTION="(DESCRIPTION=(LOAD_BALANCE=ON)(FAILOVER=ON)(ADDRESS=(PROTOCOL=TCP)(HOST=10.129.46.165)(PORT=1521))(ADDRESS=(PROTOCOL=TCP)(HOST=10.129.46.167)(PORT=1521))(LOAD_BALANCE=yes)(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=JUKUTEST)))"
 
-CONNECTION="(DESCRIPTION=(LOAD_BALANCE=ON)(FAILOVER=ON)(ADDRESS=(PROTOCOL=TCP)(HOST=livirac01n1l-vip)(PORT=1521))(ADDRESS=(PROTOCOL=TCP)(HOST=livirac01n2l-vip)(PORT=1521))(LOAD_BALANCE=yes)(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=JUKUTEST)))"
-
-DB_URL=$DB_URL DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD lein with-profiles +test-data do clear-db, update-db
+DB_URL=$CONNECTION DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD lein with-profiles +test-data do clear-db, update-db
 echo dbmaintain RETURN VALUE: $?
-
-echo -e "\n==============================================================================="
-echo "Tarkistetaan tietokannasta tuliko käännösvirheitä"
-echo -e "===============================================================================\n"
-echo "select * from user_errors; exit /" | sqlplus -L "$DB_USER/$DB_PASSWORD@$CONNECTION"
-echo sqlplus RETURN VALUE: $?
