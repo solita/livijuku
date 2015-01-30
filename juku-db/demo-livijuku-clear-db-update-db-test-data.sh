8a13b4149d5c95478508763654f0f5c8667b3a79
Petri Sirkkala <sirpete@iki.fi>
2015-01-30 15:26:56 +0200

    LIVIJUKU-102 Korjattu kantayhteyden parametrit

--- juku-db/demo-livijuku-clear-db-update-db-test-data.sh
+++ juku-db/demo-livijuku-clear-db-update-db-test-data.sh
@@ -2,5 +2,5 @@
 
 CONNECTION="letto.solita.fi:1521/ldev.solita.fi"
 
-DB_URL=$CONNECTION DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD lein with-profiles +test-data do clear-db, update-db
+DB_URL=$CONNECTION lein with-profiles +test-data do clear-db, update-db
 echo dbmaintain RETURN VALUE: $?
