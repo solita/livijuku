e157705f48acbc76a4a2c5cf03ca41da98e42c43
Mika Haapakorpi <mika.haapakorpi@gmail.com>
2015-02-03 19:57:19 +0200

    livi ympäristön testikäyttäjät

--- juku-db/test/sql/repeatable/test/users.sql
+++ juku-db/test/sql/repeatable/test/users.sql
@@ -4,3 +4,23 @@ insert into kayttaja (tunnus, etunimi, sukunimi, organisaatioid, jarjestelma)
   
 insert into kayttaja (tunnus, etunimi, sukunimi, organisaatioid, jarjestelma)
   values ('katri', 'Katri', 'Käsittelijä', (select id from organisaatio where lajitunnus = 'LV'), 0);
+
+-- roolikohtaiset testikäyttäjät livi-ympäristössä --
+-- hakijaroolit: juku_hakija, juku_allekirjoittaja
+
+insert into kayttaja (tunnus, etunimi, sukunimi, organisaatioid, jarjestelma)
+values ('juku_hakija', 'Tommi', 'Rantanen', 1, 0);
+
+insert into kayttaja (tunnus, etunimi, sukunimi, organisaatioid, jarjestelma)
+values ('juku_allekirjoittaja', 'Kimmo', 'Rantala', 1, 0);
+
+-- käsittelijäroolit: juku_kasittelija, juku_paatoksentekija
+
+insert into kayttaja (tunnus, etunimi, sukunimi, organisaatioid, jarjestelma)
+values ('juku_kasittelija', 'Tapio', 'Saarela', (select id from organisaatio where lajitunnus = 'LV'), 0);
+
+insert into kayttaja (tunnus, etunimi, sukunimi, organisaatioid, jarjestelma)
+values ('juku_paatoksentekija', 'Riikka', 'Taiminen', (select id from organisaatio where lajitunnus = 'LV'), 0);
+
+insert into kayttaja (tunnus, etunimi, sukunimi, organisaatioid, jarjestelma)
+values ('juku_paakayttaja', 'Jussi', 'Koskinen', (select id from organisaatio where lajitunnus = 'LV'), 0);
