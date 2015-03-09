b284a64a97bdec0bcff8c70e273ba5464e2c2f81
Petri Sirkkala <sirpete@iki.fi>
2015-03-09 14:06:12 +0200

    LIVIJUKU Uusia käyttäjätunnuksia allekirjoitus ja päätöksentekijä rooleihin

--- juku-db/test/sql/repeatable/test/users.sql
+++ juku-db/test/sql/repeatable/test/users.sql
@@ -1,26 +1,28 @@
 
+-- Vanha tunnus, jota ei pitäisi enää näkyä missään.
 insert into kayttaja (tunnus, etunimi, sukunimi, organisaatioid, jarjestelma)
-  values ('harri', 'Harri', 'Hakija', 1, 0);
-  
+  values ('harri', 'Harri Henrik', 'Hakija', 1, 0);
+
+-- Vanha tunnus, jota ei pitäisi enää näkyä missään.
 insert into kayttaja (tunnus, etunimi, sukunimi, organisaatioid, jarjestelma)
-  values ('katri', 'Katri', 'Käsittelijä', (select id from organisaatio where lajitunnus = 'LV'), 0);
+  values ('katri', 'Katri Karoliina', 'Käsittelijä', (select id from organisaatio where lajitunnus = 'LV'), 0);
 
 -- roolikohtaiset testikäyttäjät livi-ympäristössä --
 -- hakijaroolit: juku_hakija, juku_allekirjoittaja
 
 insert into kayttaja (tunnus, etunimi, sukunimi, organisaatioid, jarjestelma)
-values ('juku_hakija', 'Tommi', 'Rantanen', 1, 0);
+values ('juku_hakija', 'Harri', 'Hakija', 1, 0);
 
 insert into kayttaja (tunnus, etunimi, sukunimi, organisaatioid, jarjestelma)
-values ('juku_allekirjoittaja', 'Kimmo', 'Rantala', 1, 0);
+values ('juku_allekirjoittaja', 'Allu', 'Allekirjoittaja', 1, 0);
 
 -- käsittelijäroolit: juku_kasittelija, juku_paatoksentekija
 
 insert into kayttaja (tunnus, etunimi, sukunimi, organisaatioid, jarjestelma)
-values ('juku_kasittelija', 'Tapio', 'Saarela', (select id from organisaatio where lajitunnus = 'LV'), 0);
+values ('juku_kasittelija', 'Katri', 'Käsittelijä', (select id from organisaatio where lajitunnus = 'LV'), 0);
 
 insert into kayttaja (tunnus, etunimi, sukunimi, organisaatioid, jarjestelma)
-values ('juku_paatoksentekija', 'Riikka', 'Taiminen', (select id from organisaatio where lajitunnus = 'LV'), 0);
+values ('juku_paatoksentekija', 'Päivi', 'Päätöksentekijä', (select id from organisaatio where lajitunnus = 'LV'), 0);
 
 insert into kayttaja (tunnus, etunimi, sukunimi, organisaatioid, jarjestelma)
 values ('juku_paakayttaja', 'Jussi', 'Koskinen', (select id from organisaatio where lajitunnus = 'LV'), 0);
