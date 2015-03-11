05ffe567012c0f866ff0c3b945e60ecbc2599e63
Mika Haapakorpi <mika.haapakorpi@gmail.com>
2015-03-11 12:57:41 +0200

    riippuvuuksia: -[prismatic/schema "0.3.3"] -[cheshire "5.3.1"] ei tarvitse suoraan määrittää vaan nämä tulevat metosinin kirjastojen kautta

--- juku-backend/project.clj
+++ juku-backend/project.clj
@@ -5,29 +5,31 @@
 
             :dependencies [[org.clojure/clojure "1.6.0"]
                            [org.clojure/java.jdbc "0.3.6"]
+                           [slingshot "0.12.1"]
+                           [clj-time "0.8.0"]
+                           [com.google.guava/guava "18.0"]
+
+                           [environ "1.0.0"] ;; Library for managing environment variables in Clojure.
+
+                           ;; *** web application ***
+                           [http-kit "2.1.10"] ;; http server
+                           [ring "1.3.1"]
+                           [ring/ring-defaults "0.1.2"]
 
                            [metosin/compojure-api "0.17.0"]
                            [metosin/ring-swagger-ui "2.0.24"]
 
+                           ;; *** datababse ***
                            [org.clojars.zentrope/ojdbc "11.2.0.3.0"]
                            [yesql "0.5.0-beta2"]
-                           [prismatic/schema "0.3.3"]
-                           [clj-time "0.8.0"]
-                           [cheshire "5.3.1"]
-                           [ring "1.3.1"]
-                           [ring/ring-defaults "0.1.2"]
-                           [http-kit "2.1.10"]
-                           [org.clojure/tools.logging "0.3.1"]
-                           [ch.qos.logback/logback-classic "1.1.2"]
-                           [com.google.guava/guava "18.0"]
                            [com.zaxxer/HikariCP-java6 "2.2.5"]
 
-                           ;; Library for managing environment variables in Clojure.
-                           [environ "1.0.0"]
-                           ;; Timbre brings functional, Clojure-y goodness to all your logging needs.
-                           [com.taoensso/timbre "3.3.1"]
+                           ;; *** logging libraries ***
+                           [org.clojure/tools.logging "0.3.1"]
+                           [ch.qos.logback/logback-classic "1.1.2"]
 
-                           [slingshot "0.12.1"]]
+                           ;; Timbre brings functional, Clojure-y goodness to all your logging needs.
+                           [com.taoensso/timbre "3.3.1"]]
 
             :plugins [[test2junit "1.1.0"]
                       [lein-ring "0.8.12"]
