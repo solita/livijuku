3e83313015ce6174eb03ac08c465a4fb78599ca6
Mika Haapakorpi <mika.haapakorpi@gmail.com>
2015-04-15 09:46:08 +0300

    parannettu headerien validointi.

--- juku-backend/test/clj/juku/service/asiahallinta_mock.clj
+++ juku-backend/test/clj/juku/service/asiahallinta_mock.clj
@@ -1,5 +1,7 @@
 (ns juku.service.asiahallinta-mock
-  (:require [juku.user :as user]))
+  (:require [juku.user :as user]
+            [clj-time.core :as time]
+            [clj-time.format :as tf]))
 
 (def ^:dynamic *asha*)
 
@@ -10,6 +12,14 @@
 (defn request [operation] (operation *asha*))
 
 (defn valid-headers? [headers]
-  (= (get headers "SOA-KayttajanID") user/*current-user-id*))
+  (let [eq (fn[header expected-value] (= (get headers header) expected-value))
+        not-nil (fn [header] (not (nil? (get headers header))))]
+
+    (and (eq "SOA-KayttajanID" user/*current-user-id*)
+         (eq "SOA-Kutsuja" "JUKU")
+         (eq "SOA-Kohde" "ASHA")
+         (not-nil "SOA-ViestiID")
+         (not-nil "SOA-Toiminto")
+         (time/before? (tf/parse (tf/formatters :date-time) (get headers "SOA-Aikaleima")) (time/now)))))
 
 (defmacro with-asha [& body] `(binding [*asha* {}] ~@body))
