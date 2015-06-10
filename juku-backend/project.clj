c36181aac8726539b4b5c5d983130f613496e773
Mika Haapakorpi <mika.haapakorpi@gmail.com>
2015-06-10 16:19:34 +0300

    develop-haaran kehitysversio on 1.1.0-SNAPSHOT

--- juku-backend/project.clj
+++ juku-backend/project.clj
@@ -1,4 +1,4 @@
-(defproject juku-backend "0.1.0-SNAPSHOT"
+(defproject juku-backend "1.1.0-SNAPSHOT"
             :description "Liikennevirasto - Joukkoliikenteen rahoitus-, kustannus- ja suoritetietojen keräys- ja seurantajärjestelmä"
             :url "http://example.com/FIXME"
             :min-lein-version "2.4.3"
