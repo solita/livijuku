34162ad7a7e7fff21bcca7801c3ae30f9d357f92
jegranbacka <jan-erik.granbacka@solita.fi>
2015-07-09 14:17:15 +0300

    LIVIJUKU-320 Backend: Käsittelijän nimitietojen lisääminen GET /hakemuskaudet palveluun

--- juku-db/sql/repeatable/view/hakemus.sql
+++ juku-db/sql/repeatable/view/hakemus.sql
@@ -6,14 +6,14 @@ select
   case
     when (hakemus.hakemustilatunnus = 'K' and sysdate < hakuaika.alkupvm) then '0'
     else hakemus.hakemustilatunnus
-  end hakemustilatunnus,
-  hakemus.muokkausaika, hakemus.organisaatioid, 
+  end hakemustilatunnus, hakemus.muokkausaika, hakemus.organisaatioid,
   hakuaika.alkupvm hakuaika_alkupvm, hakuaika.loppupvm hakuaika_loppupvm, selite,
+  hakemus.kasittelija,
   case
     when hakemus.kasittelija is null then 'Ei määritelty'
     else concat(concat(kayttaja.etunimi, ' '), kayttaja.sukunimi)
-  end kasittelijanimi, hakemus.luontitunnus,
-  hakemus.suunniteltuavustus 
+  end kasittelijanimi,
+  hakemus.luontitunnus, hakemus.suunniteltuavustus
 from hakemus 
 inner join hakuaika 
   on hakemus.vuosi = hakuaika.vuosi and 
