55c150b5df784b262f12342d4c016904bdbef7ce
Petri Sirkkala <sirpete@iki.fi>
2015-07-09 14:46:59 +0300

    Muutettu skriptiä niin, ettei sitä ole pakko ajaa juku-db kansiosta.

--- juku-db/localhost-clean-update-testdata.sh
+++ juku-db/localhost-clean-update-testdata.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+BASEDIR=$(cd "$(dirname "$0")"; pwd)
 
-lein with-profiles +test-data uberjar
-export DB_URL=localhost:1521/orcl
-export DB_USER=juku
-export DB_PASSWORD=juku
-java -jar target/juku-db.jar clear-db
-java -jar target/juku-db.jar update-db
+(
+    cd $BASEDIR
+    lein with-profiles +test-data uberjar
+    export DB_URL=localhost:1521/orcl
+    export DB_USER=juku
+    export DB_PASSWORD=juku
+    java -jar target/juku-db.jar clear-db
+    java -jar target/juku-db.jar update-db
+)
