0ad760dbbf8690a2e2858cece339360f6a291375
jegranbacka <jan-erik.granbacka@solita.fi>
2015-07-10 09:06:42 +0300

    LIVIJUKU-320 Backend: Käsittelijän nimitietojen lisääminen GET /hakemuskaudet palveluun

--- juku-backend/test/clj/juku/service/hakemus_test.clj
+++ juku-backend/test/clj/juku/service/hakemus_test.clj
@@ -28,7 +28,7 @@
 
 (defn assoc-hakemus-defaults+ [hakemus id selite]
   (assoc (assoc-hakemus-defaults hakemus id) :luontitunnus "juku_kasittelija",
-                                             :kasittelija nil :selite selite,
+                                             :kasittelija nil, :kasittelijanimi "Ei määritelty", :selite selite,
                                              :muokkaaja nil, :lahettaja nil, :lahetysaika nil))
 
 (defn expected-hakemussuunnitelma [id hakemus haettu-avustus myonnettava-avustus]
