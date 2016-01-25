708a9505bc44f9e73ae91e0deea3a5f41e0f2663
Mika Haapakorpi <mika.haapakorpi@gmail.com>
2016-01-25 20:30:59 +0200

    LIVIJUKU-683 - ely-hakemusmuutokset - uudet määrärahatarvetyypit: - 'M', 'Muu PSA:n mukainen liikenne' - 'HK', 'Hintavelvoitteiden korvaaminen'

---
+++ juku-db/sql/incremental/03_v1.4/004_ely-hakemusmuutos.sql
@@ -0,0 +1,4 @@
+
+insert into maararahatarvetyyppi (tunnus, nimi, jarjestys) values ('M', 'Muu PSA:n mukainen liikenne', 4);
+insert into maararahatarvetyyppi (tunnus, nimi, jarjestys) values ('HK', 'Hintavelvoitteiden korvaaminen', 5);
+
