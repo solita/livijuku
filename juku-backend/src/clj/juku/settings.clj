90036cbc0e94236f91c33eae4ecd496f79844fc9
Mika Haapakorpi <mika.haapakorpi@gmail.com>
2016-02-17 12:35:07 +0200

    remove debug

--- juku-backend/src/clj/juku/settings.clj
+++ juku-backend/src/clj/juku/settings.clj
@@ -53,7 +53,5 @@
 
 (def settings (read-settings (io/file (or (env :properties-file) "./juku.properties")) default-settings Settings))
 
-(print settings)
-
 (defn asiahallinta-on? [] (not= (:asiahallinta settings) "off"))
 
