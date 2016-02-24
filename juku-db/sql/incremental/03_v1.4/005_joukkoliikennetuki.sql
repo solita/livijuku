ef68166e31d791a08ed12572e1363cbb8066188a
Mika Haapakorpi <mika.haapakorpi@gmail.com>
2016-02-24 13:58:46 +0200

    LIVIJUKU-738 - tunnuslukujen joukkoliikennetuen tietokanta

---
+++ juku-db/sql/incremental/03_v1.4/005_joukkoliikennetuki.sql
@@ -0,0 +1,15 @@
+
+create table fact_joukkoliikennetuki (
+  vuosi number(4),
+  organisaatioid not null references organisaatio (id),
+  avustuskohdeluokkatunnus not null references avustuskohdeluokka (tunnus),
+
+  tuki number(9),
+
+  constraint fact_joukkoliikennetuki_pk primary key (vuosi, organisaatioid, avustuskohdeluokkatunnus)
+);
+
+begin
+  model.define_mutable(model.new_entity('fact_joukkoliikennetuki', 'Joukkoliikennetuki fakta', 'FCTTUKI'));
+end;
+/
