4d45b4c3f8bf80773d3f92265837ecc1c36539aa
Mika Haapakorpi <mika.haapakorpi@gmail.com>
2016-03-01 18:56:06 +0200

    LIVIJUKU-743 - avustustyyppitunnukset H ja M

--- juku-backend/src/sql/tilastot.sql
+++ juku-backend/src/sql/tilastot.sql
@@ -1,6 +1,6 @@
 
 -- name: select-avustus-ah0-group-by-vuosi
-select 0 as myonnetty, hakemus.vuosi, sum(haettavaavustus) as rahamaara
+select 'H' as avustustyyppi, hakemus.vuosi, sum(haettavaavustus) as rahamaara
 from avustuskohde
   inner join hakemus on hakemus.id = avustuskohde.hakemusid
   inner join organisaatio on organisaatio.id = hakemus.organisaatioid
@@ -8,7 +8,7 @@ where hakemus.hakemustyyppitunnus = 'AH0' and
       (organisaatio.lajitunnus = :organisaatiolajitunnus or :organisaatiolajitunnus = 'ALL')
 group by hakemus.vuosi
 union all
-select 1 as myonnetty, hakemus.vuosi, sum(myonnettyavustus)
+select 'M' as avustustyyppi, hakemus.vuosi, sum(myonnettyavustus)
 from hakemus
   inner join organisaatio on organisaatio.id = hakemus.organisaatioid
   left join paatos on hakemus.id = paatos.hakemusid and voimaantuloaika is not null and poistoaika is null
