cc34b2e1394ddea2c300114433b34da37b719221
Mika Haapakorpi <mika.haapakorpi@gmail.com>
2016-03-07 14:55:13 +0200

    LIVIJUKU-755 - vakiodata muutettu samaan muotoon kuin excelissä ja tiedot päivitetty uusimman excelin pohjalta

--- juku-backend/src/clj/juku/service/tilastot.clj
+++ juku-backend/src/clj/juku/service/tilastot.clj
@@ -132,53 +132,79 @@
 
 (def avustus-data-2010-2015 {
   "KS1" {
-  ;; HSL, Oulu, Tampere, Turku
-         :organisaatiot [1, 10, 12, 13],
-         :haetut {
-                         2010 [5213000 320500  998530  831311],
-                         2011 [6230000 790000  1990872 989371],
-                         2012 [5553495 988990  2265000 1457016],
-                         2013 [8000000 2653000 3355000 3357290],
-                         2014 [8015000 3649759 3430000 4400000]
-                         2015 [5540000 5600000 3358000 6875000]}
-         :myonnetyt {
-                         2010 [5213309, 320500,  998530,  831311],
-                         2011 [6229319, 790000,  1990872, 989371],
-                         2012 [5553495, 913990,  1751951, 1457016],
-                         2013 [5821548, 1314775, 2538098, 2377717],
-                         2014 [6096802, 1751255, 2805226, 2069717]}},
+        ;; Oulu, HSL, Tampere, Turku
+        :organisaatiot
+         [10, 1, 12, 13],
+        :haetut
+         (partition 6
+           [320500  790000  988990  2653000 3649759 5600000
+            5213000 6230000 5553495 8000000 8015000 5540000
+            998530  1990872 2265000 3355000 3430000 3358000
+            831311  989371  1457016 3357290 4400000 6875000])
+        :myonnetyt
+         (partition 6
+           [320500  790000  913990  1314775 1751255 1339000
+            5213309 6229319 5553495 5821548 6096802 4700000
+            998530  1990872 1751951 2538098 2805226 2129000
+            831311  989371  1457016 2377717 2069717 1582000])}
 
   "KS2" {
-  ;; Hämeenlinna, Joensuu, Jyväskylä, Kotka, Kouvola, Kuopio, Lahti, Lappeenranta, Pori, Vaasa
-         :organisaatiot [2, 3, 4, 5, 6, 7, 8, 9, 11, 14],
-         :haetut {
-                         2010 [1359492, 363542, 1990000, 1180000, 646800, 823313, 2187624, 700000, 1180419, 732526],
-                         2011 [1477181, 437110, 1965000, 1111000, 966500, 1135340, 2389200, 754330, 1323489, 839377],
-                         2012 [1592264, 540702, 1951500, 1174000, 1123441, 1059000, 2110350, 838045, 1375860, 914610],
-                         2013 [1693099, 539238, 2041050, 977000, 982000, 1249100, 2679170, 662160, 1622061, 1031826],
-                         2014 [1459000, 741300, 5059250, 1278000, 1069000, 1249100, 6775500, 1108829, 1439534, 1281000]}
-         :myonnetyt {
-                         2010 [838383, 338622, 1294000, 864739, 560004, 722005, 1009696, 479562, 959244, 490000],
-                         2011 [951391, 377650, 1266067, 703223, 625653, 834432, 1074844, 567670, 960700, 482000],
-                         2012 [1073901, 457208, 1115100, 702635, 500230, 1152741, 1228289, 558468, 997200, 350000],
-                         2013 [985000, 515000, 1238000, 775000, 555000, 1249100, 1380000, 662160, 1162000, 395000],
-                         2014 [985000, 515000, 1238000, 775000, 555000, 1249100, 1380000, 662160, 1162000, 395000]}}
+        ;; Hämeenlinna, Joensuu, Jyväskylä, Kotka, Kouvola, Kuopio, Lahti, Lappeenranta, Pori, Vaasa
+        :organisaatiot
+         [2, 3, 4, 5, 6, 7, 8, 9, 11, 14],
+        :haetut
+         (partition 6
+           [1359492 1477181 1592264 1693099 1459000 1621130
+            363542  437110  540702  539238  741300  972500
+            1990000 1965000 1951500 2041050 5059250 1399000
+            1180000 1111000 1174000 977000  1278000 1358000
+            646800  966500  1123441 982000  1069000 1064600
+            823313  1135340 1059000 1249100 1249100 1336500
+            2187624 2389200 2110350 2679170 6775500 3873300
+            700000  754330  838045  662160  1108829 1211000
+            1180419 1323489 1375860 1622061 1439534 1483436
+            732526  839377  914610  1031826 1281000 1250717])
+        :myonnetyt
+         (partition 6
+           [838383  951391 1073901 985000 985000 946000
+            338622  377650 457208 515000 515000 505000
+            1294000 1266067 1115100 1238000 1238000 1213000
+            864739  703223 702635 775000 775000 728000
+            560004  625653 500230 555000 555000 544000
+            722005  834432 1152741 1249100 1249100 1174000
+            1009696 1074844 1228289 1380000 1380000 1352000
+            479562  567670 558468 662160 662160 636000
+            959244  960700 997200 1162000 1162000 1092000
+            490000  482000 350000 395000 395000 390000])}
 
   "ELY" {
-  ;; Pohjois-Pohjanmaa, Pohjois-Savo, Varsinais-Suomi, Uudenmaa, Etelä-Pohjanmaa, Kaakkois-Suomi, Keski-Suomi, Lappi, Pirkanmaa
-         :organisaatiot [23, 20, 17, 16, 22, 18, 21, 24, 19],
-         :haetut {
-                         2010 [4726300, 8946000, 5044972, 5949922, 3589635, 3940058, 3425700, 3416000, 2988258],
-                         2011 [6190000, 10225000, 5885000, 8301474, 4308816, 4419818, 4405000, 3980000, 4392405],
-                         2012 [5740000, 9525222, 6900000, 11100000, 4340000, 5440000, 3960000, 3990000, 4120000],
-                         2013 [6026000, 7826094, 6985000, 6461517, 4113803, 1450669, 2535000, 4165000, 3762000],
-                         2014 [4983361, 6775000, 2980000, 9590919, 3664548, 1705803, 1764000, 3987000, 2415000]}
-         :myonnetyt {
-                         2010 [4577000, 8653000, 4756000, 6272000, 3264000, 3539000, 3186000, 3284000, 3073000],
-                         2011 [4423000, 8594000, 4802000, 6225000, 3579000, 3662000, 3183000, 3293000, 3036000],
-                         2012 [4209000, 8250000, 4803000, 6303000, 3507000, 3457000, 3003000, 3324000, 3141000],
-                         2013 [4100000, 6900000, 2800000, 4600000, 3450000, 1450000, 1900000, 3350000, 2200000],
-                         2014 [3879000, 6695000, 2586000, 5095000, 3192000, 3065000, 1684000, 3138000, 1985000]}}})
+        ;; Pohjois-Pohjanmaa, Pohjois-Savo, Varsinais-Suomi, Uudenmaa, Etelä-Pohjanmaa, Kaakkois-Suomi, Keski-Suomi, Lappi, Pirkanmaa
+        :organisaatiot
+         [23, 20, 17, 16, 22, 18, 21, 24, 19],
+        :haetut
+         (partition 7
+           [4726300 6190000  5740000 6026000 0  5083361 4272000
+            8946000 10225000 9525222 7826094 0  6775000 6750000
+            5044972 5885000  6900000 6985000 0  2980000 2620000
+            5949922 8301474  11100000 6461517 0 9590918 11720374
+            3589635 4308816  4340000 4113803 0  3664548 4208284
+            3940058 4419818  5440000 1450669 0  1705803 1331000
+            3425700 4405000  3960000 2535000 0  1764000 1903150
+            3416000 3980000  3990000 4165000 0  3987000 3737782
+            2988258 4392405  4120000 3762000 0  2415000 2744314])
+        :myonnetyt
+         (partition 7
+            [4577000 4423000 4209000 4100000 3879000 3986000 3974000
+             8653000 8594000 8250000 6900000 6695000 6420000 6325000
+             4756000 4802000 4803000 2800000 2586000 2557000 2702000
+             6272000 6225000 6303000 4600000 5095000 5518000 5617000
+             3264000 3579000 3507000 3450000 3192000 3156000 3551000
+             3539000 3662000 3457000 1450000 3065000 4150000 2781000
+             3186000 3183000 3003000 1900000 1684000 1590000 1760000
+             3284000 3293000 3324000 3350000 3138000 3053000 3086000
+             3073000 3036000 3141000 2200000 1985000 1963000 2033000])}})
+
+(def vuodet (range 2010 2017))
 
 (def avustus-data+all
   (assoc avustus-data-2010-2015 "ALL" (apply m/deep-merge-with #(apply concat %) (vals avustus-data-2010-2015))))
@@ -186,19 +212,23 @@
 (def sum (partial reduce +))
 
 (defn data->avustus-tilasto [data]
-  (concat (map #(cons "H" %) (m/map-values sum (:haetut data)))
-          (map #(cons "M" %) (m/map-values sum (:myonnetyt data)))))
+  (concat
+    (map vector (repeat "H") vuodet (apply mapv (comp sum vector) (:haetut data)))
+    (map vector (repeat "M") vuodet (apply mapv (comp sum vector) (:myonnetyt data)))))
 
 (def avustus-tilasto-2010-2015
   (m/map-values data->avustus-tilasto avustus-data+all))
 
+(def avustus-tilasto-ely-2016
+  {:haetut (nth (coll/find-first #(and (= (nth % 0) "H") (= (nth % 1) 2016)) (get avustus-tilasto-2010-2015 "ELY")) 2)
+   :myonnetyt (nth (coll/find-first #(and (= (nth % 0) "M") (= (nth % 1) 2016)) (get avustus-tilasto-2010-2015 "ELY")) 2)})
+
 (defn data->avustus-tilasto-organisaatio [data]
-  (mapcat #(map vector
-             (:organisaatiot data)
-             (repeat %)
-             (get (:haetut data) %)
-             (get (:myonnetyt data) %))
-       (range 2010 2015)))
+  (apply concat
+         (map-indexed #(map vector (repeat %2) vuodet
+                            (nth (:haetut data) %1)
+                            (nth (:myonnetyt data) %1))
+                        (:organisaatiot data))))
 
 (def avustus-tilasto-organisaatio-2010-2015
   (m/map-values data->avustus-tilasto-organisaatio avustus-data+all))
@@ -207,13 +237,24 @@
   (cons (first new-data) ;; header
     (concat old-data (rest new-data))))
 
+(defn if-all-add-ely-2016 [organisaatiolajitunnus data]
+  (if (= organisaatiolajitunnus "ALL")
+    (map (fn [row]
+           (if (= (nth row 1) 2016)
+             (case (nth row 0)
+               "H" (update row 2 (partial + (:haetut avustus-tilasto-ely-2016)))
+               "M" (update row 2 (partial + (:myonnetyt avustus-tilasto-ely-2016))))
+             row)) data)
+    data))
+
 (defn avustus-tilasto [organisaatiolajitunnus]
-  (include-old-data
-    (get avustus-tilasto-2010-2015 organisaatiolajitunnus)
-    (if (= organisaatiolajitunnus "KS3")
-      (select-avustus-ks3-group-by-vuosi {} {:as-arrays? true :connection db})
-      (select-avustus-group-by-vuosi (c/bindings->map organisaatiolajitunnus)
-                                     {:as-arrays? true :connection db}))))
+  (if-all-add-ely-2016 organisaatiolajitunnus
+    (include-old-data
+      (get avustus-tilasto-2010-2015 organisaatiolajitunnus)
+      (if (= organisaatiolajitunnus "KS3")
+        (select-avustus-ks3-group-by-vuosi {} {:as-arrays? true :connection db})
+        (select-avustus-group-by-vuosi (c/bindings->map organisaatiolajitunnus)
+                                       {:as-arrays? true :connection db})))))
 
 (defn avustus-organisaatio-tilasto [organisaatiolajitunnus]
   (include-old-data
