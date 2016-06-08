eb8b1cbfb00357bb3f88b91ada0fccc7e035bcf5
Mika Haapakorpi <mika.haapakorpi@gmail.com>
2016-06-08 15:55:29 +0300

    LIVIJUKU-881 - organisaatiot järjestetään seuraavasti: decode(lajitunnus, 'KS1', 1, 'KS2', 2, 'KS3', 3, 'ELY', 4, 0), nimi

--- juku-backend/src/sql/organisaatio.sql
+++ juku-backend/src/sql/organisaatio.sql
@@ -1,6 +1,7 @@
 
 -- name: select-organisaatiot
 select id, nimi, lajitunnus, pankkitilinumero from organisaatio
+order by decode(lajitunnus, 'KS1', 1, 'KS2', 2, 'KS3', 3, 'ELY', 4, 0), nimi
 
 -- name: select-organisaatio-like-exttunnus
 select id from organisaatio where :tunnus like lower(exttunnus)
