5abe2ad5286bfdd4ec204192650edb2d68d41af3
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2017-12-22 12:15:57 +0200

    LIVIJUKU-1022 - minor version bump

--- juku-backend/project.clj
+++ juku-backend/project.clj
@@ -1,4 +1,4 @@
-(defproject juku-backend "1.6.2"
+(defproject juku-backend "1.6.3"
             :description "Liikennevirasto - Joukkoliikenteen rahoitus-, kustannus- ja suoritetietojen keräys- ja seurantajärjestelmä"
             :url "https://extranet.liikennevirasto.fi/juku/"
             :min-lein-version "2.4.3"
