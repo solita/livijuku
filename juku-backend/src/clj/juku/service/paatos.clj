4b508480c1cfe5f1c72773d90c8da2640107272a
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2018-12-17 18:13:19 +0200

    LIVIJUKU-1042 - ELY-päätöstekstit 2019

--- juku-backend/src/clj/juku/service/paatos.clj
+++ juku-backend/src/clj/juku/service/paatos.clj
@@ -65,6 +65,7 @@
   (case (:hakemustyyppitunnus hakemus)
     "AH0" (str "paatos-ah0-" (str/lower-case (:lajitunnus organisaatio)) "-2018.txt")
     "ELY" (case (:vuosi hakemus)
+            2019 "paatos-ely-2019.txt"
             2018 "paatos-ely-2018.txt"
             2017 "paatos-ely-2017.txt"
             "paatos-ely-2016.txt")
