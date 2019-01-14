ddab37db2f3f0ca906ea9d0b0c36cab37c2c86f4
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2019-01-14 13:16:38 +0200

    LIVIJUKU-1043 - lisätty traficom organisaatio + e2e test data fixes + e2e test hakemuskausi on 2020

---
+++ juku-db/sql/incremental/05_v1.6/001_traficom.sql
@@ -0,0 +1,2 @@
+
+insert into organisaatio (id, nimi, lajitunnus, exttunnus) values (100, 'Traficom', 'LV', 'Traficom%');
--- juku-db/test/sql/repeatable/test/kausi-2017.sql
+++
@@ -1,8 +0,0 @@
-
-insert into hakemuskausi (vuosi) values (2018);
-
-insert into hakuaika (vuosi,hakemustyyppitunnus,alkupvm,loppupvm) values (2018, 'AH0', to_date('01.09.2016','DD.MM.YYYY'), to_date('15.12.2017','DD.MM.YYYY'));
-insert into hakuaika (vuosi,hakemustyyppitunnus,alkupvm,loppupvm) values (2018, 'MH1', to_date('01.07.2017','DD.MM.YYYY'), to_date('31.08.2017','DD.MM.YYYY'));
-insert into hakuaika (vuosi,hakemustyyppitunnus,alkupvm,loppupvm) values (2018, 'MH2', to_date('01.01.2018','DD.MM.YYYY'), to_date('31.01.2018','DD.MM.YYYY'));
-insert into hakuaika (vuosi,hakemustyyppitunnus,alkupvm,loppupvm) values (2018, 'ELY', to_date('01.09.2016','DD.MM.YYYY'), to_date('31.10.2016','DD.MM.YYYY'));
-
