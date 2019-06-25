56013896637a9833e66679e85a67d918dbd510d5
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2019-06-26 00:37:18 +0300

    LIVIJUKU-1033 - orgnisaatioiden ulkoiset tunnukset

--- juku-db/sql/repeatable/organisaatio.sql
+++ juku-db/sql/repeatable/organisaatio.sql
@@ -1,5 +1,46 @@
 
-update organisaatio set exttunnus = '069205/069206' where id = 1;
-update organisaatio set exttunnus = '050930/%' where id = 100;
+-- KS1 --
+update organisaatio set exttunnus = '069205/%' where nimi = 'Helsingin seudun liikenne';
+update organisaatio set exttunnus = '050053/%' where nimi = 'Oulu';
+update organisaatio set exttunnus = '050834/%' where nimi = 'Tampere';
+update organisaatio set exttunnus = '050836/%' where nimi = 'Turku';
 
+-- KS2 --
+update organisaatio set exttunnus = '050820/%' where nimi = 'Hämeenlinna';
+update organisaatio set exttunnus = '050822/%' where nimi = 'Joensuu';
+update organisaatio set exttunnus = '050878/%' where nimi = 'Jyväskylä';
+update organisaatio set exttunnus = '050824/%' where nimi = 'Kotka';
+update organisaatio set exttunnus = '050838/%' where nimi = 'Kouvola';
+update organisaatio set exttunnus = '050855/%' where nimi = 'Kuopio';
+update organisaatio set exttunnus = '050842/%' where nimi = 'Lahti';
+update organisaatio set exttunnus = '050880/%' where nimi = 'Lappeenranta';
+update organisaatio set exttunnus = '050070/%' where nimi = 'Pori';
+update organisaatio set exttunnus = '050851/%' where nimi = 'Vaasa';
 
+-- KS3 --
+update organisaatio set exttunnus = '050830/%' where nimi = 'Rovaniemi';
+-- update organisaatio set exttunnus = '051059/%' where nimi = 'Kajaani'; -- duplicates
+-- update organisaatio set exttunnus = where nimi = 'Meri-Lappi'; -- missing
+-- update organisaatio set exttunnus = where nimi = 'Kokkola'; -- missing
+update organisaatio set exttunnus = '050882/%' where nimi = 'Seinäjoki';
+-- update organisaatio set exttunnus = where nimi = 'Rauma'; -- missing
+update organisaatio set exttunnus = '066684/%' where nimi = 'Salo';
+-- update organisaatio set exttunnus = where nimi = 'Hyvinkää'; -- missing
+update organisaatio set exttunnus = '050864/%' where nimi = 'Riihimäki';
+-- update organisaatio set exttunnus = where nimi = 'Mikkeli'; -- missing
+update organisaatio set exttunnus = '050832/%' where nimi = 'Savonlinna';
+-- update organisaatio set exttunnus = where nimi = 'Imatra'; -- missing
+
+-- ELY --
+update organisaatio set exttunnus = '060432/%' where nimi = 'Uusimaa ELY';
+update organisaatio set exttunnus = '060448/%' where nimi = 'Varsinais-Suomi ELY';
+update organisaatio set exttunnus = '060434/%' where nimi = 'Kaakkois-Suomi ELY';
+update organisaatio set exttunnus = '060439/%' where nimi = 'Pirkanmaa ELY';
+update organisaatio set exttunnus = '061054/%' where nimi = 'Pohjois-Savo ELY';
+update organisaatio set exttunnus = '060436/%' where nimi = 'Keski-Suomi ELY';
+update organisaatio set exttunnus = '061060/%' where nimi = 'Etelä-Pohjanmaa ELY';
+update organisaatio set exttunnus = '060673/%' where nimi = 'Pohjois-Pohjanmaa ELY';
+update organisaatio set exttunnus = '060671/%' where nimi = 'Lappi ELY';
+
+-- LV --
+update organisaatio set exttunnus = '050930/%' where nimi = 'Traficom';
